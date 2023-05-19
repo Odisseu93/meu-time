872484c24cdc4b0e4f77e06fcdcc78164c2103db
Ulisses <ulissessuporteti01@gmail.com>
2023-05-18 23:12:59 -0300

    feat: add input:submit on login form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,12 +6,15 @@ const Login: React.FC = () => {
 	
 	return (
 		<main className="grid">
-			<div className="wrapper-input w-2/3 md:max-w-lg mx-auto mt-8">
-				<label className='block text-center mb-1' htmlFor='apiKey'>Insira a chave da API</label>
-				<BallIcon cls='wrapper-input__ball absolute m-1' />
-				<input className="wrapper-input__input w-full h-9 ps-9 border border-bd_input_default rounded-sm focus:border-bd_input_hover" type="text" name="apiKey" id="apiKey" />
-				<p className='mt-2 text-sm  ps-2'><a  href="https://dashboard.api-football.com/register" className='text-violet-100 underline active:text-blue-900' target="_blank" rel="noopener noreferrer">Toque aqui</a> para obter a chave.</p>
-			</div>
+			<form className="grid md:grid-cols-2 gap-4 md:gap-2 items-center w-2/3 border-2  mx-auto md:ms-[25vw] mt-8">
+				<div className="wrapper-input md:w-min[422px]">
+					<label className='block text-center mb-1' htmlFor='apiKey'>Insira a chave da API</label>
+					<BallIcon cls='wrapper-input__ball absolute m-1' />
+					<input className="wrapper-input__input w-full h-9 ps-9 border border-bd_inp_text_login_default rounded-sm focus:border-bd_inp_text_login_active" type="text" name="apiKey" id="apiKey" />
+					<p className='mt-2 text-sm ps-2'><a  href="https://dashboard.api-football.com/register" className='text-violet-100 underline active:text-blue-900' target="_blank" rel="noopener noreferrer">Toque aqui</a> para obter a chave.</p>
+				</div>
+				<input className='w-[105px] h-fit p-2 border-2 mx-auto md:mx-0 rounded-md bg-violet-200 text-white  md:hover:bg-violet-300  active:bg-violet-400 cursor-pointer' type="submit" value="Enviar" />
+			</form>
 			<div className="wrapper-svg-soccer ms-auto">
 				<SoccerImage cls="wrapper-svg-soccer__svg animate-violet mt-[60%]" />
 			</div>
