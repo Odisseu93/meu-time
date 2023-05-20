0adb39de972a935d368f2a8b0cb46c1f0035807f
Ulisses <ulissessuporteti01@gmail.com>
2023-05-20 17:28:19 -0300

    feat: Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
+import Dashboard from './pages/dasboard';
 
 function App() {
 
 	return (
 		<Routes>
 			<Route path='/login' element={<Login />} />
+			<Route path='/dashboard' element={<Dashboard />} />
 		</Routes>
 	)
 }
---
+++ src/pages/dasboard/index.tsx
@@ -0,0 +1,21 @@
+import { useAuth } from '@/context/auth/hook';
+import { getDataSStorage } from '@/util/storage';
+
+const Dashboard: React.FC = () => {
+	const { isLoggedIn } = useAuth();
+
+	return (
+		isLoggedIn || getDataSStorage('ak') ? (
+			<main className="w-full h-[90vh] mt-6 border-y border-dashed bg-bg_main_dashboard">
+				<h1>Seja bem vindo!</h1>;
+			</main>
+		)
+			: (
+				<main className="w-full h-[90vh] mt-6 border-y border-dashed bg-bg_main_dashboard">
+					<h1>Você não tem acesso, efutue o login!</h1>;
+				</main>
+			)
+	);
+};
+
+export default Dashboard;
