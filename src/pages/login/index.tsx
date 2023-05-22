d75742d7ca899f357d2298d034a85242c872dc87
Ulisses <ulissessuporteti01@gmail.com>
2023-05-22 11:25:02 -0300

    feat(login): add animation on button:submit

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,13 +3,14 @@ import SoccerImage from '@/components/SoccerImage';
 import { useAuth } from '@/context/auth/hook';
 import FootballApi from '@/services/api';
 import { setDataSStorage } from '@/util/storage';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
 const Login: React.FC = () => {
 	const { setIsLoggedIn, isLoggedIn } = useAuth();
 	const navigate = useNavigate();
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		isLoggedIn &&
@@ -24,14 +25,17 @@ const Login: React.FC = () => {
 			const inp = e.currentTarget.querySelector('[name=apiKey]') as HTMLInputElement;
 
 			const footballApi = new FootballApi(inp.value);
+			setIsLoading(true);
 			footballApi.getStatus().then(res => {
 				if(res instanceof Error){
 					setIsLoggedIn(false);
 					alert('Chave inválida!');
+					setIsLoading(false);
 					return
 				}
 			 setDataSStorage('ak',inp.value);
 			 setIsLoggedIn(true);
+			 setIsLoading(false);
 			 navigate('/dashboard');
 			});
 
@@ -44,11 +48,21 @@ const Login: React.FC = () => {
 			<form onSubmit={(e)=> handleSubmit(e)} className="grid md:grid-cols-2 gap-4 md:gap-2 items-center w-2/3 border-2  mx-auto md:ms-[25vw] mt-8">
 				<div className="wrapper-input md:w-min[422px]">
 					<label className='block text-center mb-1' htmlFor='apiKey'>Insira a chave da API</label>
-					<BallIcon cls='wrapper-input__ball absolute m-1' />
+					<BallIcon cls={`${isLoading ? 'animate-spin': ''} wrapper-input__ball absolute m-1`} />
 					<input className="wrapper-input__input w-full h-9 ps-9 border border-bd_inp_text_login_default rounded-sm focus:border-bd_inp_text_login_active" type="text" name="apiKey" id="apiKey" />
 					<p className='mt-2 text-sm ps-2'><a  href="https://dashboard.api-football.com/register" className='text-violet-100 underline active:text-blue-900' target="_blank" rel="noopener noreferrer">Toque aqui</a> para obter a chave.</p>
 				</div>
-				<input className='w-[105px] h-fit p-2 border-2 mx-auto md:mx-0 rounded-md bg-violet-200 text-white  md:hover:bg-violet-300  active:bg-violet-400 cursor-pointer' type="submit" value="Enviar" />
+				<button className="btn-input w-[105px] h-fit p-2 border-2 mx-auto md:mx-0 rounded-md bg-violet-200 text-white  md:hover:bg-violet-300  active:bg-violet-400">
+					{
+						isLoading ?
+							(
+								<span className='submit-loading animate-pulse grid w-fit' aria-live="polite" aria-busy="true">
+									Aguarde...</span>
+							)
+							:
+							<span className='submit-default' aria-live="polite" aria-busy="false">Enviar</span>
+					}	
+				 </button>
 			</form>
 			<div className="wrapper-svg-soccer ms-auto">
 				<SoccerImage cls="wrapper-svg-soccer__svg animate-violet mt-[60%]" />
