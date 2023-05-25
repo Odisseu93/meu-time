bb55d8ffe404bb2c4b1e35afccbb00184dd0d928
Ulisses <ulissessuporteti01@gmail.com>
2023-05-25 16:22:05 -0300

    reafactor(FormSelect): remove unecessarry prop handleChange

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,4 +1,4 @@
-import Select, { SingleValue } from 'react-select';
+import Select from 'react-select';
 
 
 type Options = {
@@ -15,24 +15,19 @@ interface FormSelectProps {
 		clsSelect?: string,
 	},
 	label: string,
-	handleChange?: (e: SingleValue<{
-		value: string,
-		label: string,
-	}>) => void,
+	isRequired: boolean,
 }
 
-const FormSelect: React.FC<FormSelectProps> = ({ options, name, classNames, label, handleChange }) => {
+const FormSelect: React.FC<FormSelectProps> = ({ options, name, classNames, label, isRequired }) => {
 
 	return (
 		<div className={classNames['clsWrapper'] ?? 'form-gorup grid gap-1 mx-auto w-[200px]'}>
-			<label className={classNames['clsLabel']?? 'text-violet-400 text-center ps-1 text-2xl'}>{label}</label>
+			<label className={classNames['clsLabel'] ?? 'text-violet-400 text-center ps-1 text-xl'}>{label}</label>
 			<Select className={classNames['clsSelect'] ?? 'text-violet-400'}
 				name={name}
-				onChange={handleChange ? (e: SingleValue<{
-					value: string,
-					label: string,
-				}>
-				) => handleChange(e): undefined} options={options} />
+				options={options}
+				required={isRequired}
+			/>
 		</div>
 	)
 }
