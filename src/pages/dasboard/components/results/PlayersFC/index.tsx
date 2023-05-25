bfb50c91e726f98a91a4db94d0501b5b91a73b94
Ulisses <ulissessuporteti01@gmail.com>
2023-05-25 16:46:00 -0300

    chore : change PlayersFC style

--- src/pages/dasboard/components/results/PlayersFC/index.tsx
+++ src/pages/dasboard/components/results/PlayersFC/index.tsx
@@ -6,11 +6,12 @@ const PlayersFC: React.FC<{data: Player[] }> = ({data: players}) => {
 	return (
 		<article
 			className="info-players
-       flex gap-4 flex-wrap justify-center 
-       w-11/12 overflow-auto
-       my-3 mx-auto bg-[#9c27f538]
+			 w-full 
+       flex gap-2 flex-wrap justify-center 
+       md:w-11/12 overflow-auto
+       my-3 mx-auto p-2 bg-[#9c27f538]
        border-2 border-solid border-bd_inp_text_login_default border-opacity-90
-       rounded-md p-2  shadow-inner"
+       rounded-md md:p-2  shadow-inner"
 		>
 			<h3
 				className="w-full text-xl font-bold text-center text-violet-400">
